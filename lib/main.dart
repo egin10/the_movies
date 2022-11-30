d0d0f6a6e157ac4b0d8ab219e4eb07b9f22d0f56
egin10 <egin066@gmail.com>
2022-11-30 14:18:43 +0700

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'ui/views/movies/now_playing_movie/now_playing_movie_viewmodel.dart';
 import 'ui/views/movies/popular_movie/popular_movie_viewmodel.dart';
 import 'ui/views/movies/top_rated_movie/top_rated_movie_viewmodel.dart';
 import 'ui/views/movies/upcoming_movie/upcoming_movie_viewmodel.dart';
+import 'ui/views/splash_screen/splash_screen_view.dart';
 import 'ui/views/tv_shows/airing_today_tv_show/airing_today_tv_show_viewmodel.dart';
 import 'ui/views/tv_shows/detail_tv_show/detail_tv_show_viewmodel.dart';
 import 'ui/views/tv_shows/on_the_air_tv_show/on_the_air_tv_show_viewmodel.dart';
@@ -47,18 +48,17 @@ class MyApp extends StatelessWidget {
             backgroundColor: CustomColor.darkBlueColor,
           ),
         ),
-        home: const MainView(),
-        // home: FutureBuilder(
-        //   future: Future.delayed(
-        //     const Duration(seconds: 3),
-        //   ),
-        //   builder: (context, snapshot) {
-        //     if (snapshot.connectionState == ConnectionState.done) {
-        //       return const MainView();
-        //     }
-        //     return const SplashScreenView();
-        //   },
-        // ),
+        home: FutureBuilder(
+          future: Future.delayed(
+            const Duration(seconds: 3),
+          ),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return const MainView();
+            }
+            return const SplashScreenView();
+          },
+        ),
       ),
     );
   }
